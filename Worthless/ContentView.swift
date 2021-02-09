986b1588219bbf8c5c667de59028f70a0a607241
Yauheni Shauchenka <y.shauchenka@gmail.com>
2021-02-09 15:16:39 +0300

    Initial project commit

---
+++ Worthless/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Worthless
+//
+//  Created by Yauheni Shauchenka on 2/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Worthless world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Worthless/WorthlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorthlessApp.swift
+//  Worthless
+//
+//  Created by Yauheni Shauchenka on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct WorthlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
