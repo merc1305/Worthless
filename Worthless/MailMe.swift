fd29622e5a4dc2c88157be3697705033a2e81293
Yauheni Shauchenka <y.shauchenka@gmail.com>
2022-03-08 01:29:22 +0300

    MailMe git conflict resolving

---
+++ Worthless/MailMe.swift
@@ -0,0 +1,117 @@
+//
+//  mailMe.swift
+//  Worthless
+//
+//  Created by Yauheni Shauchenka on 3/7/22.
+//
+
+import SwiftUI
+import MessageUI
+
+struct MailView: UIViewControllerRepresentable {
+
+    @Binding var isShowing: Bool
+    @Binding var result: Result<MFMailComposeResult, Error>?
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+
+        @Binding var isShowing: Bool
+        @Binding var result: Result<MFMailComposeResult, Error>?
+
+        init(isShowing: Binding<Bool>,
+             result: Binding<Result<MFMailComposeResult, Error>?>) {
+            _isShowing = isShowing
+            _result = result
+        }
+
+        func mailComposeController(_ controller: MFMailComposeViewController,
+                                   didFinishWith result: MFMailComposeResult,
+                                   error: Error?) {
+            defer {
+                isShowing = false
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShowing: $isShowing,
+                           result: $result)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.mailComposeDelegate = context.coordinator
+        vc.setToRecipients(["y.shauchenka+wless@gmail.com"])
+        vc.setSubject("Решил отправить денег, помоги понять как")
+        vc.setMessageBody("Привет, отличное приложение, однако вот что ещё сюда можно добавить, что бы убедить меня стать китом: \n", isHTML: true)
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {
+
+    }
+}
+
+struct MailMe: View {
+    
+    @State var result: Result<MFMailComposeResult, Error>? = nil
+    @State var isShowingMailView = false
+    
+    var body: some View {
+//        VStack {
+//            Text("Ок, давай поговорим, как насчет электронной почты? Тапай на кнопку и напиши всё, что обо мне думаешь, не стесняйся!")
+//                .multilineTextAlignment(.leading)
+//                .padding()
+//
+//            if MFMailComposeViewController.canSendMail() {
+//                           Button("Написать Жене") {
+//                               self.isShowingMailView.toggle()
+//                           }
+//                       } else {
+//                           Text("Упс! Похоже это устройство не может отправлять письма, попробуй написать напрямую на y.shauchenka+wless@gmail.com")
+//                               .multilineTextAlignment(.center)
+//                               .padding()
+//                       }
+//                       if result != nil {
+//                           Text("Result: \(String(describing: result))")
+//                               .lineLimit(nil)
+//                       }
+//        }
+        
+        VStack {
+            
+            Text("Ок, давай поговорим, как насчет электронной почты? Тапай на кнопку и напиши всё, что обо мне думаешь, не стесняйся!")
+                .multilineTextAlignment(.leading)
+                .padding()
+            
+            if MFMailComposeViewController.canSendMail() {
+                Button("Написать Жене") {
+                    self.isShowingMailView.toggle()
+                }
+            } else {
+                Text("Упс! Похоже это устройство не может отправлять письма, попробуй написать напрямую на y.shauchenka+wless@gmail.com")
+            }
+//            if result != nil {
+//                Text("Result: \(String(describing: result))")
+//                    .lineLimit(nil)
+//            }
+        }
+        .sheet(isPresented: $isShowingMailView) {
+            MailView(isShowing: self.$isShowingMailView, result: self.$result)
+        }
+        
+        
+    }
+}
+
+struct mailMe_Previews: PreviewProvider {
+    static var previews: some View {
+        MailMe()
+    }
+}
